dc93c630c90e36c6cf12c914db1a5f09fe9af14d
Blake Perti <blakeperti4@gmail.com>
2026-02-13 17:14:37 -0800

    basic start template

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+
+  return (
+    <h1>basic start</h1>
+  );
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
 import './index.css'
 import App from './App.jsx'
 
