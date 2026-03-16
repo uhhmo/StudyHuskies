09fd9fb7193a79deb15dcb3282f3160ab0ca2826
Blake Perti <blakeperti4@gmail.com>
2026-03-15 20:40:13 -0700

    few checklist requirements

--- src/pages/quiz.jsx
+++ src/pages/quiz.jsx
@@ -7,7 +7,7 @@ import QuizReview from '../components/QuizReview';
 import QuizStats from '../components/QuizStats';
 import wizard from '../assets/wizard.png';
 import QuizMode from '../components/QuizMode';
-import { Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router';
 
 
 function Quiz() {
@@ -17,7 +17,8 @@ function Quiz() {
             <section className="mb-5 p-4 border rounded shadow-sm text-center">
                 <h1>Adventure Quiz</h1>
                 <p>Test your knowledge on an adventure! Explore with interactive flashcards, but be careful,
-                    get a question wrong and you'll be sent back to the checkpoint!.</p>
+                    get a question wrong and you'll be sent back to the checkpoint! Study your missed cards at 
+                    the end (or from the studying page)!</p>
             </section>
 
             <Outlet />
